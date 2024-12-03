9c1f21fe093db4c9fcb68f218fda830b7fa0ae87
TheGhossst <nandukrishnapkm97@gmail.com>
2024-12-03 22:14:07 +0530

    feat: scrollToTop function

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,6 +3,13 @@ import { Link } from "react-router-dom"
 
 export function Footer() {
 
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+        });
+    };
+
     return (
         <footer className="border-t-2 border-gray-700 py-8 bottom-0 left-0 right-0" id="contact">
             <div className="container mx-auto px-4">
@@ -25,29 +32,18 @@ export function Footer() {
                         <Linkedin className="h-6 w-6 text-gray-400 group-hover:text-white transition-colors duration-200" />
                         <span className="mt-2 text-sm text-gray-400 group-hover:text-white transition-colors duration-200">LinkedIn</span>
                     </a>
-                    {/*<a 
-                        href="./Resume.pdf" 
-                        target="_blank" 
-                        rel="noopener noreferrer" 
-                        className="flex flex-col items-center group"
-                    >
-                        <FileText className="h-6 w-6 text-gray-400 group-hover:text-white transition-colors duration-200" />
-                        <span className="mt-2 text-sm text-gray-400 group-hover:text-white transition-colors duration-200">Résumé</span>
-                    </a>*/}
                     <Link to="/resume" className='flex flex-col items-center group'>
                         <FileText className="h-6 w-6 text-gray-400 group-hover:text-white transition-colors duration-200" />
                         <span className="mt-2 text-sm text-gray-400 group-hover:text-white transition-colors duration-200">Résumé</span>
                     </Link>
-                    <a
-                        href = '/'
+                    <button
+                        onClick={scrollToTop}
                         className="ml-auto text-white p-2 absolute bottom-2 right-0"
                     >
                         <ArrowUp className="h-6 w-6" />
-                    </a>
-
+                    </button>
                 </div>
             </div>
         </footer>
     );
 }
-
